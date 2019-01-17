d5da957df5339532dca4d74366cd5f13e37475c2
Desiree Solomon <d.d.solomon4@gmail.com>
2019-01-17 18:25:56 -0500

    Updates

---
+++ precache-manifest.1cb109e49b3cae4635fb8b02cb1796f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fe4c2eb150fb6fdd85f46bfa2a07898",
+    "url": "/clicky-game/static/media/morocco-blue.0fe4c2eb.png"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "9026948862a1e917df29",
+    "url": "/clicky-game/static/js/main.90269488.chunk.js"
+  },
+  {
+    "revision": "93f38d1637645ebc29d1",
+    "url": "/clicky-game/static/js/1.93f38d16.chunk.js"
+  },
+  {
+    "revision": "9026948862a1e917df29",
+    "url": "/clicky-game/static/css/main.183afeb8.chunk.css"
+  },
+  {
+    "revision": "17e6d990d3ff685f0e8b88e70d3d1452",
+    "url": "/clicky-game/index.html"
+  }
+];
